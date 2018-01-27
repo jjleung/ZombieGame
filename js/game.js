41fc887ac691bde21af2c3579d40c7fb9104bdd8
Jesse J H Leung <Marduk@Jesses-MacBook-Air.local>
2018-01-27 13:03:20 -1000

    marged rotating with tracking enemy bullets

--- js/game.js
+++ js/game.js
@@ -5,11 +5,13 @@
   const GAME_CONTAINER_ID = 'game';
   const GFX = 'gfx';
   const INITIAL_MOVESPEED = 4;
-  const SQRT_TWO = Math.sqrt(2);
   const PLAYER_BULLET_SPEED = 6;
   const ENEMY_SPAWN_FREQ = 100;
-  const randomGenerator = new Phaser.RandomDataGenerator();
   const ENEMY_SPEED = 4.5;
+  const ENEMY_FIRE_FREQ = 30;
+  const ENEMY_BULLET_ACCEL = 100;
+  const SQRT_TWO = Math.sqrt(2);
+  const randomGenerator = new Phaser.RandomDataGenerator();
   
   const game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, GAME_CONTAINER_ID, {preload, create, update});
 
@@ -17,6 +19,7 @@
   let cursors;
   let playerBullets;
   let enemies;
+  let enemyBullets;
 
   function preload(){
     game.load.spritesheet(GFX, '../assets/shmup-spritesheet-140x56-28x28-tile.png', 28, 28);
@@ -32,6 +35,8 @@
     player.anchor.setTo(0.5,0.5);
     playerBullets = game.add.group();
     enemies = game.add.group();
+    enemyBullets = game.add.group();
+    enemyBullets.enableBody = true;
 
   };
 
@@ -62,18 +67,21 @@
         player.angle += 4;
         break;
     }
-    switch( true ){
-      case cursors.down.isDown:
-        // player.y += player.moveSpeed;
-        break;
-      case cursors.up.isDown:
-        player.y -= player.moveSpeed;
-        break;
-    }
+    // switch( true ){
+    //   case cursors.down.isDown:
+    //     player.y += player.moveSpeed;
+    //     break;
+    //   case cursors.up.isDown:
+    //     player.y -= player.moveSpeed;
+    //     break;
+    // }
   };
 
   function handlePlayerFire() {
     playerBullets.add(game.add.sprite(player.x, player.y, GFX, 7));
+    enemyBullets.children.forEach( bullet => {
+      game.physics.arcade.accelerateToObject(bullet, player, ENEMY_BULLET_ACCEL);
+    });
   };
 
   function handleBulletAnimations(){
@@ -111,7 +119,16 @@
 
       enemiesHit.forEach( destroyEnemy );
     }
-  };
+
+    // check if enemy bullets hit the player
+    let enemyBulletsLanded = enemyBullets.children
+    .filter( bullet => bullet.overlap(player));
+  
+    if (enemyBulletsLanded.length) {
+      handlePlayerHit();
+      enemyBulletsLanded.forEach( removeBullet );
+    }
+  }
 
   //behavior functions
   function randomlySpawnEnemy() {
@@ -123,7 +140,17 @@
 
   function handleEnemyActions() {
     enemies.children.forEach( enemy => enemy.y += ENEMY_SPEED );
-  };
+    enemies.children.forEach( enemy => randomEnemyFire(enemy));
+  }
+
+  function randomEnemyFire(enemy) {
+    if (randomGenerator.between(0, ENEMY_FIRE_FREQ) === 0) {
+      let enemyBullet = game.add.sprite(enemy.x, enemy.y, GFX, 9);
+      enemyBullet.checkWorldBounds = true;
+      enemyBullet.outOfBoundsKill = true;
+      enemyBullets.add( enemyBullet );
+    }
+  }
 
   //utility functions
   function cleanup() {
@@ -143,6 +170,9 @@
   function gameOver() {
     game.state.destroy();
     game.add.text(90, 200, 'YOUR HEAD ASPLODE', { fill: '#FFFFFF' });
-  };
+    let playAgain = game.add.text(120, 300, `Play Again`, { fill: `#FFFFFF` });
+    playAgain.inputEnabled = true;
+    playAgain.events.onInputUp.add(() => window.location.reload());
+  }
 
 })(window.Phaser);
